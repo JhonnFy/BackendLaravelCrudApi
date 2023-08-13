ea8e9213834f9618da69c0db52ff170a002b5b7b
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-08-13 17:51:34 -0500

    ViewIndex

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel CRUD With Multiple Image Upload</title>
+
+      <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!-- Font-awesome -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    </head>
+    <body>
+
+        <div class="container" style="margin-top: 80px;">
+
+            <h3 class="text-center text-danger"><b>Laravel Crud Customer</b> </h3>
+            <a href="/create" class="btn btn-outline-success">Add New Customer</a>
+
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Company Name</th>
+                    <th>Addres</th>
+                    <th>Phone_number</th>
+                    <th>Record</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+   
+                </tbody>
+              </table>
+        </div>
+
+
+
+
+    </body>
+</html>
