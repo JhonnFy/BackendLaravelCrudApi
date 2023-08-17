f4d5cfbaea291d55f5708fbd1ff7a568d3046e46
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-08-17 07:53:28 -0500

    ChangeRouteController

--- app/Http/Controllers/API/CrudController.php
+++ app/Http/Controllers/API/CrudController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use app\http\Controllers\Controller;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\http\Controllers\CrudController;
+use App\Http\Controllers\API\CrudController;
 
 /*
 |--------------------------------------------------------------------------
