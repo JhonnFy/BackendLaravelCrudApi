359bed39dbedbf14a00638a6d7072bd8d3b98d16
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-08-17 08:49:45 -0500

    ChangeRouteController

--- app/Http/Controllers/API/CrudController.php
+++ app/Http/Controllers/API/CrudController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Customer;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CrudController extends Controller
 {
